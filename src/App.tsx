3454166c69fba64a021322fabf672e9ffa1842b1
firoz.nur <firoznur5@gmail.com>
2025-07-02 21:40:25 +0600

    Add : Increment And Decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <>
       <h1>Counter With Redux</h1>
-      <button>Increment</button>
+      <button onClick={handleIncrement}>Increment</button>
       <div>0</div>
-      <button>Increment</button>
+      <button onClick={handleDecrement}>Increment</button>
     </>
   );
 }
