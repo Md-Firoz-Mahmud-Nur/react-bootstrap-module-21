f9192ead484c34286f403d486f117723342b2f41
firoz.nur <firoznur5@gmail.com>
2025-07-02 21:08:03 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Counter With Redux</h1>
+      <button>Increment</button>
+      <div>0</div>
+      <button>Increment</button>
+    </>
+  );
+}
+
+export default App;
