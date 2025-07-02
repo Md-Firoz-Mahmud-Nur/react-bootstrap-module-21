678a746c564eab3605173eb528b0d911d8ddd1b8
firoz.nur <firoznur5@gmail.com>
2025-07-03 01:15:51 +0600

    Add : Dynamic Increment & Decrement

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,21 @@ function App() {
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state: RootState) => state.counter);
 
-  const handleIncrement = () => {
-    dispatch(increment());
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
   };
-  const handleDecrement = () => {
-    dispatch(decrement());
+  const handleDecrement = (amount: number) => {
+    dispatch(decrement(amount));
   };
 
   return (
     <>
       <h1>Counter With Redux</h1>
-      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={() => handleIncrement(5)}>Increment By 5</button>
+      <button onClick={() => handleIncrement(1)}>Increment</button>
       <div>{count}</div>
-      <button onClick={handleDecrement}>Increment</button>
+      <button onClick={() => handleDecrement(5)}>Decrement By 5</button>
+      <button onClick={() => handleDecrement(1)}>Decrement</button>
     </>
   );
 }
